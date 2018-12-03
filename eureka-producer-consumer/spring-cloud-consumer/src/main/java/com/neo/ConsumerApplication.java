141409310324eaab998d1762b5128961c199ebfb
王恩 <wehappy888@163.com>
2018-12-03 10:46:45 +0800

    test

--- eureka-producer-consumer/spring-cloud-consumer/src/main/java/com/neo/ConsumerApplication.java
+++ eureka-producer-consumer/spring-cloud-consumer/src/main/java/com/neo/ConsumerApplication.java
@@ -3,7 +3,6 @@ package com.neo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
@@ -11,10 +10,9 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
 @EnableFeignClients
 public class ConsumerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConsumerApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class, args);
+    }
 
 
 }
